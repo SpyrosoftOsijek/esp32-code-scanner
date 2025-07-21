87ec7c4c1ea522d4ffa5b77f96050490ac966b2f
vjekoslav.pecar <vjekoslav.pecar@gmail.com>
2025-07-21 12:00:35 +0200

    Added OpenCV header files

---
+++ components/component-barcode_detector/component_barcode_detector.cpp
@@ -0,0 +1,7 @@
+#include "include/opencv/modules/objdetect/include/opencv2/objdetect/barcode.hpp"
+#include "include/component_barcode_detector.hpp"
+#include <stdio.h>
+#include <mat.hpp>
+
+
+
---
+++ components/component-barcode_detector/include/component_barcode_detector.hpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <mat.hpp>
+
+
+class BarcodeDetector {
+
+    public:
+        int detect(cv::InputArray image);
+
+};
