9113883d9119db76aa2196b85b33af55c442d21b
squash332 <filip.hulak12@gmail.com>
2025-07-21 12:03:56 +0200

    added cam component - to be finished

--- main/main.cpp
+++ main/main.cpp
@@ -13,40 +13,28 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
-extern "C" void app_main(void)
-{
-    printf("Hello world!\n");
+#include "camera_component.hpp"
+#include "esp_log.h"
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
+extern "C" void app_main() {
+    Camera cam;
+    if (!cam.init()) {
+        ESP_LOGE("MAIN", "Camera failed to initialize");
+        while(true)  {
+            
+        }
         return;
     }
- 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    while (true) {
+        camera_fb_t* fb = cam.capture();
+        if (fb) {
+            ESP_LOGI("MAIN", "Captured frame, size: %d bytes", fb->len);
+            cam.release(fb);
+        } else {
+            ESP_LOGE("MAIN", "Capture failed");
+        }
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(2000));  // 2s delay cuz monitor went crazy
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
