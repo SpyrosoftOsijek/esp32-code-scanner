cca8bf26fab7f99077a388bb7556c679081585c5
vjekoslav.pecar <vjekoslav.pecar@gmail.com>
2025-07-25 13:12:27 +0200

    Renamed component

--- components/barcode_reader/barcode_reader.cpp
+++ components/barcode_reader/barcode_reader.cpp
@@ -1,5 +1,5 @@
 #include "include/opencv/modules/objdetect/include/opencv2/objdetect/barcode.hpp"
-#include "include/component_barcode_detector.hpp"
+#include "include/barcode_reader.hpp"
 #include <stdio.h>
 #include <mat.hpp>
 
