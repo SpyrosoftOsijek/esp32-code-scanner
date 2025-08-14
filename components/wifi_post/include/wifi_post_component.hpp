0354a318c688a2603077ead6682569cfdb2d35b7
squash332 <filip.hulak12@gmail.com>
2025-08-14 14:14:22 +0200

    rename + added post webhook

--- components/wifi_post/include/wifi_post_component.hpp
+++ components/wifi_post/include/wifi_post_component.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -9,18 +9,14 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-
-
-
+#include "esp_http_client.h"
+#include "cJSON_Utils.h"
 
 #define MAX_FAILURES 10
-
 
 // WiFi connection result flags for event group
 #define WIFI_SUCCESS BIT0
 #define WIFI_FAILURE BIT1
-
-
 
 /**
  * @brief Connect to the configured WiFi Access Point (STA mode).
@@ -31,3 +27,4 @@
  * @return ESP_OK (0) on success, or ESP_FAIL (-1) on failure
  */
 esp_err_t connect_wifi(void);
+void send_barcode_post(const char *barcode);
--- main/main.cpp
+++ main/main.cpp
@@ -4,31 +4,38 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include "wifi_component.hpp"
+#include "wifi_post_component.hpp"
 #include "freertos/task.h"
 #include <inttypes.h>
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_log.h"
 
-extern "C" void app_main() {
-    
-    esp_err_t status = WIFI_FAILURE;
+extern "C" void app_main()
+{
 
-    // init storage
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+  esp_err_t status = WIFI_FAILURE;
 
-    // connect to wireless AP
-	status = connect_wifi();
-	if (WIFI_SUCCESS != status)
-	{
-		ESP_LOGI("WIFI", "Failed to associate to AP, dying...");
-		return;
-	}
+  // init storage
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 
+  // connect to wireless AP
+  status = connect_wifi();
+  if (WIFI_SUCCESS != status)
+  {
+    ESP_LOGI("WIFI", "Failed to associate to AP, dying...");
+    return;
+  }
+
+  const char *test_barcode = "12345";
+
+  send_barcode_post(test_barcode);
+
+  vTaskDelay(pdMS_TO_TICKS(1000)); 
 }
